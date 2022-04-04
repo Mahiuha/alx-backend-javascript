a7fbbad85fcbe7f8d25066c4a87ad253e4ccf5d7
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-05 00:34:05 +0300

    modified the functions inside textBlock so they are overwritten inside the condition

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
