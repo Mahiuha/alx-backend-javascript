0add5cc37a28c40db7f2f5ef1cab98c7c9c1a2a7
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-05 01:36:26 +0300

    minor bug fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
