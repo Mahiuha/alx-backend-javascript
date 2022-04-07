6cc90691cc735d3ec4207983ba0c0bdd602b6e03
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-07 22:46:43 +0300

    class that extends another

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,10 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {}
+}
