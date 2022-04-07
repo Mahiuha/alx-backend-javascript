f18f4671006c4966f25a00d200f0b1e3415df501
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-07 23:04:49 +0300

    testing

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,19 +4,7 @@ export default class Airport {
     this._code = code;
   }
 
-  set name(name) {
-    this._name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set code(code) {
-    this._code = code;
-  }
-
-  get code() {
-    return this._code;
+  toString() {
+    return `[object ${this._code}]`;
   }
 }
