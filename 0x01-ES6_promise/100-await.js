3935f4ddd4e10fd27b0bcf3fa48600b1c4a07f5d
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-12 00:09:48 +0300

    an await function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const resFromUploadPhoto = await uploadPhoto();
+    const resFromCreateUser = await createUser();
+    return {
+      photo: resFromUploadPhoto,
+      user: resFromCreateUser,
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
