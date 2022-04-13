cd479c8896df0f010828073329e716823dce3e12
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-13 05:28:16 +0300

    typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const arr = new Int8Array(buffer);
+  arr[position] = value;
+  return arr;
+}
