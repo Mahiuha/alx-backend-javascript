a59048599b166896a892a9bb3dfb15e5a115980f
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-13 05:31:27 +0300

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,6 @@
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const el of set) { if (el && el.startsWith(startString)) res += `${el.slice(startString.length)}-`; }
+  return res.slice(0, res.length - 1);
+}
