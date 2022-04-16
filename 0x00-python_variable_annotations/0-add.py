c3597b449610af0dac842d3adc35612ade67cbff
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-16 23:18:36 +0300

    adding two float numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" 
+0. Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ 
+    Adds two float number numbers together.
+    """
+    return a + b
