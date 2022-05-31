ec108410cd10419382f8610327a07545477320a3
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 01:15:17 +0300

    a small HTTP server using the http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
