5589a4dd8ced72df9e9acd7f3d75317e5c390bd6
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 01:58:13 +0300

    code refactor

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  if (url === '/') res.end('Hello Holberton School!');
-  else if (url === '/students') {
-    countStudents(process.argv[2])
-      .then((message) => {
-        const response = `This is the list of our students\n${message}`;
-        res.end(response);
-      })
-      .catch((err) => {
-        res.end(`${err.message}\n`);
-      });
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.end(`${students.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
+    }
   }
+  res.end();
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
