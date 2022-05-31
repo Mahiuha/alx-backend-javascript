eb777a098683af28e1bb83d6cdc2f2b3f224952f
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 02:02:21 +0300

    code refactor

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,25 +1,41 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const port = 1245;
-
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
+const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(process.argv[2]);
-      res.end(`${students.join('\n')}`);
-    } catch (err) {
-      res.end(err.message);
-    }
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2])
+        .then((v) => {
+          res.write(`Number of students: ${v.counter}\n`);
+          let len = 0;
+          for (const key in v.inFields) {
+            if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
+              res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
+              if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
+              len += 1;
+            }
+          }
+          res.end();
+        })
+        .catch(() => {
+          res.writeHead(404);
+          res.write('This is the list of our students\n');
+          res.end('Cannot load the database');
+        });
+      break;
+    default:
+      res.writeHead(404);
+      res.write('This is the list of our students\n');
+      res.end(('Cannot load the database'));
   }
-  res.end();
 });
-
-app.listen(port);
+app.listen(1245);
 
 module.exports = app;
