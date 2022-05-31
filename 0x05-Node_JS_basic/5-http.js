5bdbe115e84f41e09e59cce92e84f2d9a7912338
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 02:19:54 +0300

    code refactor

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,37 @@
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  switch (req.url) {
-    case '/':
-      res.writeHead(200);
-      res.end('Hello Holberton School!');
-      break;
-    case '/students':
-      res.writeHead(200);
-      res.write('This is the list of our students\n');
-      countStudents(process.argv[2])
-        .then((v) => {
-          res.write(`Number of students: ${v.counter}\n`);
-          let len = 0;
-          for (const key in v.inFields) {
-            if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
-              res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
-              if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
-              len += 1;
-            }
-          }
-          res.end();
-        })
-        .catch(() => {
-          res.writeHead(404);
-          res.write('This is the list of our students\n');
-          res.end('Cannot load the database');
-        });
-      break;
-    default:
-      res.writeHead(404);
-      res.write('This is the list of our students\n');
-      res.end(('Cannot load the database'));
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
-app.listen(1245);
+
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+
+});
 
 module.exports = app;
