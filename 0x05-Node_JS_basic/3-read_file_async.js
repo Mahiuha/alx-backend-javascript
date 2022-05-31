dd2e03a89e585163da7d26a3c2640ce5b364f310
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 02:03:11 +0300

    code refactor

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,29 @@
+
+  
 const fs = require('fs');
 
 function countStudents(path) {
-  const promise = (resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, dataDb) => {
-      if (err) reject(Error('Cannot load the database'));
-      if (dataDb) {
-        const inFields = {}; // {field: {counter: # of students, students: [list of students]}}
-        const data = dataDb.split('\n');
-        console.log(`Number of students: ${data.length - 1}`);
-
-        for (let i = 1; i < data.length; i += 1) {
-          const line = data[i].split(','); // get each word
-          if (inFields[line[3]]) { // line[3] is the name field
-            inFields[line[3]].counter += 1;
-            inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-          } else {
-            inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
-          }
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(Error('Cannot load the database'));
+      if (data) {
+        let newData = data.toString().split('\n');
+        newData = newData.slice(1, newData.length - 1);
+        console.log(`Number of students: ${newData.length}`);
+        const obj = {};
+        newData.forEach((el) => {
+          const student = el.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
         }
-        for (const key in inFields) {
-          if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-            console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-          }
-        }
-        // ret obj of students in fields and number of students
-        resolve({ inFields, counter: data.length - 1 });
       }
+      res();
     });
   };
   return new Promise(promise);
 }
+
 module.exports = countStudents;
