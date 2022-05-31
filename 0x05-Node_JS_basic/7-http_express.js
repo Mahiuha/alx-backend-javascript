51a7621495b3748980b4212240f3122a03f0399c
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 02:09:00 +0300

    code refactor

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,22 +2,15 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', async (req, res) => {
-  countStudents(process.argv[2])
-    .then((message) => {
-      const response = `This is the list of our students\n${message}`;
-      res.send(response);
-    })
-    .catch((err) => {
-      res.send(`${err.message}`);
-    });
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
 app.listen(1245);
-
 module.exports = app;
