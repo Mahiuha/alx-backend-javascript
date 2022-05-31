64ebc88389d71736526f51dd771a484e449e11e3
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 01:56:52 +0300

    code refactor

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,29 @@
+
+  
 const fs = require('fs');
 
-module.exports = async function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+function countStudents(path) {
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(Error('Cannot load the database'));
+      if (data) {
+        let newData = data.toString().split('\n');
+        newData = newData.slice(1, newData.length - 1);
+        console.log(`Number of students: ${newData.length}`);
+        const obj = {};
+        newData.forEach((el) => {
+          const student = el.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
+        }
       }
-      const fields = {};
-      const students = data.split('\n').map((student) => student.split(','));
-      students.shift();
-      const message = [];
-
-      message.push(`Number of students: ${students.length}`);
-      students.forEach((student) => {
-        if (!fields[student[3]]) fields[student[3]] = [];
-        fields[student[3]].push(student[0]);
-      });
-      Object.keys(fields).forEach((key) => {
-        message.push(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
-      });
-      const result = message.join('\n');
-      console.log(result);
-      resolve(result);
+      res();
     });
-  });
-};
+  };
+  return new Promise(promise);
+}
+
+module.exports = countStudents;
