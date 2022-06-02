c05d35b5aa8a7f263689979cf5e6fbd1dd0d35fc
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-03 00:09:21 +0300

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,27 +1,18 @@
-function calculateNumber(type, a, b) {
-  if (isNaN(a) || isNaN(b)) {
-      throw new TypeError();
-  }
-    if (type == 'SUM') {
-    return Math.round(a) + Math.round(b);	
-  } else if (type == 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  } else if (type == 'DIVIDE') {
-    try {
-      if (Math.round(b) == 0) throw 'Error';
+'use strict';
+const calculateNumber = (type, a, b) => {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+      break;
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+      break;
+    case 'DIVIDE':
+      if (Math.round(b) === 0) return 'Error';
       return Math.round(a) / Math.round(b);
-    }
-    catch(err) {
-      return (err);
-    }
-  }
+      break;
+    default:
+      break;
+  };
 }
 module.exports = calculateNumber;
-
-// console.log(calculateNumber('SUM', -3.7, 5));
-// console.log(calculateNumber('SUBTRACT', -3.7, 5));
-// console.log(calculateNumber('DIVIDE', -3.7, 50));
-// console.log(calculateNumber('SUM', 1.4, 4.5));
-// console.log(calculateNumber('SUBTRACT', 1.4, 4.5));
-// console.log(calculateNumber('DIVIDE', 1.4, 4.5));
-// console.log(calculateNumber('DIVIDE', 1.4, 0));
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,77 +1,38 @@
-const assert = require("assert");
+'use strict';
+const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
-describe('calculateNumber', function () {
-    describe('sum', function () {
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUM', 3, 4), 7);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUM', 3.4, 4.2), 7);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUM', 3.0, 4.0), 7);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUM', -3, 4), 1);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUM', 3, - 4), -1);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUM', -3.4, 4.5), 2);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUM', -3.5, 2), -1);
-	});
-    });
-    describe('subtract', function () {
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', 3, 4), -1);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', 3.4, 4.2), -1);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', 3.0, 4.0), -1);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', -3, 4), -7);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', 3, -4), 7);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', -3.4, 4.5), -8);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('SUBTRACT', -3.5, 2), -5);
-	});
-    });
-    describe('divide', function () {
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', 3, 4), 0.75);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', 3.4, 4.2), 0.75);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', 3.0, 4.0), 0.75);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', -3, 4), -0.75);
-	});
-	it('should work without rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', 3, -4), -0.75)
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', -3.4, 4.5), -0.6);
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', -3.5, 2), -1.5)
-	});
-	it('should work with rounding', function () {
-	    assert.equal(calculateNumber('DIVIDE', -3.5, .4), 'Error')
-	});
-    });
-})
+describe('calculateNumber type == SUM', () => {
+  it('checks the output', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber('SUM', 4.5, 1.4), 6);
+    assert.strictEqual(calculateNumber('SUM', 0.0, 0), 0);
+    assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
+    assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
+    assert.strictEqual(calculateNumber('SUM', -1, -1), -2);
+  });
+});
+
+describe('calculateNumber type == SUBTRACT', () => {
+  it('checks the output', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
+    assert.strictEqual(calculateNumber('SUBTRACT', 3.1, 2.5), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 5), -5);
+    assert.strictEqual(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, 1), -2);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 0), -1);
+  });
+});
+
+describe('calculateNumber type == DIVIDE', () => {
+  it('check the output', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 2, 2.5), 0.6666666666666666);
+    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
+    assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
+    assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+  });
+});
