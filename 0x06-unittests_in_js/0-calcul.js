3eb122b45803ef11cfb49e0d212a83f0ac7f1b61
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-02 23:59:04 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,12 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+      throw new TypeError();
+  }
+  return Math.round(a) + Math.round(b);
+}
 module.exports = calculateNumber;
 
+console.log(calculateNumber(2, 3))
+console.log(calculateNumber(1.2, 3.3));
+console.log(calculateNumber(2.5, 3.4));
+console.log(calculateNumber(10.2, 11.3));
